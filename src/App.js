6dd5bd717cd729a8fd5508a9e9fb309bdcc4928b
Felipe <fquiceno1@misena.edu.co>
2022-02-24 16:59:42 -0500

    conectamos los hooks con props AddUserForm con Add.js, creamos la funcion onSubmit para atrapar los datos del formulario, estos datos los identificamos con name y los convertimos a objeto con la funcion onSubmit

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
       <h1>CRUD App with hooks</h1>
       <div className="flex-row">{/*columna 1 */}
         <h2>Add user</h2>
-        <AddUserForm />
+        <AddUserForm addUser={addUser}/>
       </div>
       <div className='flex-large'>{/*columna 2 */}
         <h2>View users</h2>
--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import { useForm } from 'react-hook-form' //llamamos la libreria para trabajar con formularios
 
-const AddUserForm = () => {
+const AddUserForm = (props) => {
+    
+    /*
+     * Creamos una funcion de Estado, de la libreria para manejar datos de formulario,
+    con esta libreria podemos validar los campos e imprimir errores de
+    validaciones. handleSubmit nos sirve para agregar nuestro formulario
+     con el name del formulario nosotros podemos enviarlo al objeto
+    */
+    //State
+    const{register, errors, handleSubmit} = useForm();
+    
+    /*
+    onSubmit es una funcion para enviar los datos que hay en los
+    campos del formulario,esta funcion se va a utilizar en la etiqueta 
+    form
+    */
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+
     return (  
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
         <label>Name</label>
-        <input type="text" name="name" value="" />
+        <input type="text" name="name"  />
         <label>Username</label>
-        <input type="text" name="username" value="" />
+        <input type="text" name="username"  />
         <button>Add new user</button>
         </form>
         
