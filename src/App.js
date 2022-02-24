19b39b446ab8af964819a309ca59ce263a335862
Felipe <fquiceno1@misena.edu.co>
2022-02-24 15:01:22 -0500

    Agregamos la tala userTable al app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import UserTable from './components/UserTable';
 
 
 function App() {
   return (
-    <div className='container'>
+    <div className='container'> 
       <h1>CRUD App with hooks</h1>
       <div className="flex-row">{/*columna 1 */}
         <h2>Add user</h2>
       </div>
       <div className='flex-large'>{/*columna 2 */}
         <h2>View users</h2>
+        <UserTable/>{/*Agregamos codigo tabla de usertable */}
       </div>
     </div>
   );
