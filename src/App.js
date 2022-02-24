53d29a2eb6e4c37f895d7354814e14b37a53e64c
Felipe <fquiceno1@misena.edu.co>
2022-02-24 14:48:14 -0500

    agregamos container con dos columnas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 
 function App() {
   return (
-    <div>Hola mundo</div>
+    <div className='container'>
+      <h1>CRUD App with hooks</h1>
+      <div className="flex-row">{/*columna 1 */}
+        <h2>Add user</h2>
+      </div>
+      <div className='flex-large'>{/*columna 2 */}
+        <h2>View users</h2>
+      </div>
+    </div>
   );
 }
 
