6983fa84a3070a4f4dd3ee0c3f9d066815006b33
Felipe <fquiceno1@misena.edu.co>
2022-02-24 17:51:18 -0500

    Agregamos la validacion de campos con la libreria react-hook-formy agregamos una funcion en onSubmit que lÃimpia los campos una vez le demos al boton de agregar usuariio

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -10,24 +10,40 @@ const AddUserForm = (props) => {
      con el name del formulario nosotros podemos enviarlo al objeto
     */
     //State
-    const{register, errors, handleSubmit} = useForm();
-    
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     /*
     onSubmit es una funcion para enviar los datos que hay en los
     campos del formulario,esta funcion se va a utilizar en la etiqueta 
     form
     */
-    const onSubmit = (data) => {
+    const onSubmit = (data, e) => {
         console.log(data)
+
+        //Limpiamos campos despues de darle al boton enviar
+        e.target.reset();
     }
 
 
     return (  
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>{/* hacemos uso del onSubmit para enviar los datos del formulario y convertirlos a objeto*/}
         <label>Name</label>
-        <input type="text" name="name"  />
+
+        <input type="text" name="name"
+        {...register("name", { required: true })}/>{/* Preguntamos  que el campo name debe ser requerido, No puede
+                                                        estar vacio y si lo esta, imprimimos en un div un mensaje de error*/}
+        <div>
+            {errors.name && <span>Este campo es requerido</span>}    
+        </div>
+
+
         <label>Username</label>
-        <input type="text" name="username"  />
+        <input type="text" name="username" 
+            {...register("username", { required: true })}/>{/* Preguntamos  que el campo name debe ser requerido, No puede
+                                                                estar vacio y si lo esta, imprimimos en un div un mensaje de error*/}
+        <div>
+            {errors.username && <span>Este campo es requerido</span>}    
+        </div>     
+
         <button>Add new user</button>
         </form>
         
